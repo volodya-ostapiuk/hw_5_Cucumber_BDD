258068f574ebd7a8caa1c62c4cd7db341c2c89fa
Volodymyr Ostapiuk <Volodymyr_Ostapiuk@epam.com>
2020-09-13 14:36:38 +0300

    Changed testng.xml

--- src/test/java/com/epam/runner/RunnerTest.java
+++ src/test/java/com/epam/runner/RunnerTest.java
@@ -1,11 +1,35 @@
 package com.epam.runner;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/com.epam/features/gmail.feature",
         glue = {"com/epam/steps"})
 public class RunnerTest {
+    private TestNGCucumberRunner cucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        cucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(dataProvider = "features")
+    public void feature(CucumberFeatureWrapper featureWrapper) {
+        cucumberRunner.runCucumber(featureWrapper.getCucumberFeature());
+    }
+
+    @DataProvider(parallel = true)
+    public Object[][] features() {
+        return cucumberRunner.provideFeatures();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        cucumberRunner.finish();
+    }
 }
